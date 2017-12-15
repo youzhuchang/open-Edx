97faf1b386a9d910bf7b5ca0eadc2901b5d61463
localstepdo <stepdo@microsoft.com>
2017-12-15 08:13:01 -0800

    temporary wrapper for onebox.sh

---
+++ tmp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Copyright (c) Microsoft Corporation. All Rights Reserved.
+# Licensed under the MIT license. See LICENSE file on the project webpage for details.
+
+# cloud:
+sudo bash onebox.sh --msft-oauth prod
+# local: -d hokiePokie , -r ds
