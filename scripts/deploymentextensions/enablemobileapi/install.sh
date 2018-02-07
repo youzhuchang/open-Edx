8cc7dd6da4de1f4ba6b4f99e99e8fbd04b437952
Eric Meng (Shang Hai Wei Chuang Ruan Jian) <v-wemen@microsoft.com>
2018-02-07 09:39:36 +0800

    修改 azure cli 2.0 设置环境变量

--- scripts/deploymentextensions/enablemobileapi/install.sh
+++ scripts/deploymentextensions/enablemobileapi/install.sh
@@ -157,11 +157,12 @@ execute_remote_command()
 authenticate-azureuser()
 {
     # this call requires azure cli2
+    az cloud set --name AzureChinaCloud
 
     # login
-    results=`az login -e AzureChinaCloud -u $aad_webclient_id --service-principal --tenant $aad_tenant_id -p $aad_webclient_appkey --output json`
+    results=`az login -u $aad_webclient_id --service-principal --tenant $aad_tenant_id -p $aad_webclient_appkey --output json`
     exit_on_error "Could not login to azure with the provided service principal credential from '${HOSTNAME}' !" "${error_mobilerestapi_update_failed}" "${notification_email_subject}" "${cluster_admin_email}"
-
+    
     # select the appropriate subscription to set the execution context
     az account set --subscription "${azure_subscription_id}"
     exit_on_error "Could not set the azure subscription context to ${azure_subscription_id} from '${HOSTNAME}' !" "${error_mobilerestapi_update_failed}" "${notification_email_subject}" "${cluster_admin_email}"
